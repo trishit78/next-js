7e32130b3648a66b343dab50f212ceb42daff0ed
Trishit Bhowmik <90523629+trishit78@users.noreply.github.com>
2025-04-30 19:38:55 +0530

    params and search params

---
+++ routing-demo/src/app/(auth)/forgotpassword/page.tsx
@@ -0,0 +1,5 @@
+export default function forgotPassword(){
+    return (
+        <h1>Forgotten Password</h1>
+    )
+}
--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home(){
-    return <h1>Welcome Home!</h1>
-};
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome Home!</h1>
+      <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
+      <Link href="/articles/breaking-news-123?lang=es">Read in Spanish</Link>
+      <Link href="/articles/breaking-news-123?lang=fr">Read in French</Link>
+    </>
+  );
+}
